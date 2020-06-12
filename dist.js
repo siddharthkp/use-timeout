44b3e41b5d0e63fe97f9cf968ddc5a90028bef30
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-06-12 12:52:54 +0200

    initial commit

--- dist.js
+++ dist.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
+//
+
 function _getRequireWildcardCache() {
   if (typeof WeakMap !== "function") return null;
   var cache = new WeakMap();

1aadca04c09e38a0da6e1118b3e4b28ebdee0c90
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-06-12 12:36:34 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from "react";
 
 function useTimeout(callback, delay) {
-  const savedCallback = useRef()
+  const savedCallback = useRef();
 
   // Remember the latest callback.
-  useEffect(
-    () => {
-      savedCallback.current = callback
-    },
-    [callback]
-  )
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-  // Set up the interval.
-  useEffect(
-    () => {
-      function tick() {
-        savedCallback.current()
-      }
-      if (delay !== null) {
-        const id = setTimeout(tick, delay)
-        return () => clearTimeout(id)
-      }
-    },
-    [delay]
-  )
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setTimeout(tick, delay);
+      return () => clearTimeout(id);
+    }
+  }, [delay]);
 }
 
-export default useTimeout
+export default useTimeout;
